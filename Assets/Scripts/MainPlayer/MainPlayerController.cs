5b28565683dabaac719b356a99ae14fb68d04540
Nguyễn Duy Nhất <nguyenduynhat.250505@gmail.com>
2025-11-15 14:23:58 +0700

    Add player anim, player controller, map

---
+++ Assets/Scripts/MainPlayer/MainPlayerController.cs
@@ -0,0 +1,77 @@
+using UnityEngine;
+
+public class MainPlayerController : MonoBehaviour
+{
+    public bool FacingLeft { get { return facingLeft; } set { facingLeft = value; } }
+
+    [SerializeField] private float moveSpeed = 1f;
+
+    private MainPlayerInput playerControls;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+    private Animator myAnimator;
+    private SpriteRenderer mySpriteRender;
+
+    private bool facingLeft = false;
+
+    private void Awake()
+    {
+        playerControls = new MainPlayerInput();
+        rb = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        mySpriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        PlayerInput();
+    }
+
+    private void FixedUpdate()
+    {
+        AdjustPlayerFacingDirection();
+        Move();
+    }
+
+    private void PlayerInput()
+    {
+        movement = playerControls.Movement.Move.ReadValue<Vector2>();
+
+        myAnimator.SetFloat("moveX", movement.x);
+        myAnimator.SetFloat("moveY", movement.y);
+    }
+
+    private void Move()
+    {
+        rb.MovePosition(rb.position + movement * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    private void AdjustPlayerFacingDirection()
+    {
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 playerScreenPoint = Camera.main.WorldToScreenPoint(transform.position);
+
+        if (mousePos.x < playerScreenPoint.x)
+        {
+            mySpriteRender.flipX = true;
+            FacingLeft = true;
+        }
+        else
+        {
+            mySpriteRender.flipX = false;
+            FacingLeft = false;
+        }
+    }
+}
